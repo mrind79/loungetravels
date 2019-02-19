bf033e02964ea30469bef338ddc26c937de39fd3
Mrinmoy Debnath <mrind79@gmail.com>
2019-02-19 13:26:11 +0530

    first set of project files

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
+
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AfService } from './providers/af.service';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreModule } from 'angularfire2/firestore';
+import { AdminGuard } from './guards/admin.guard';
+import { SubscriberGuard } from './guards/subscriber.guard';
+
+import { MaterialModule } from './material.module';
+import { MenusService } from './service/menus/menus.service';
+import { PostsService } from './service/posts/posts.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    MaterialModule
+  ],
+  providers: [AfService, AdminGuard, SubscriberGuard, MenusService, PostsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
